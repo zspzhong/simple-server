fa9e1dff441968f0a1eb8cf862227cc62c736b85
shasharoman <shasharoman@gmail.com>
2016-05-29 21:53:48 +0800

    delete no need file

--- bin/webApp.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-node /root/simple-server/lib/webApp.js -f /root/simple-server/conf/config.json -c $1 1>>/root/simple-server/logs/access.log 2>>/root/simple-server/logs/error.log
